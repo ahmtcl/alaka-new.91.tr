6dc0c3ed9d037e9c3814b03915dfb711b7a62dcc
ahmtcl <ahmtcl3@gmail.com>
2026-07-02 13:34:22 +0300

    firebase-admin serverExternalPackages olarak eklendi

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["firebase-admin"],
   images: {
     formats: ["image/webp", "image/avif"],
     remotePatterns: [
