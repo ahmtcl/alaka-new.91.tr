b2ab11e36c5ddb4dbd400bd24fc4673bdb1c5e9e
ahmtcl <ahmtcl3@gmail.com>
2026-07-08 15:59:59 +0300

    next.config: storage.googleapis.com domain eklendi

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "*.firebasestorage.app",
       },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+      },
     ],
   },
 };
